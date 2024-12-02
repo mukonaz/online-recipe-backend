864db364c9b133c868fafe716b03a3ede1edf65a
Mushe <mosescolince161@gmail.com>
2024-12-02 16:05:45 +0200

    front end

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    ingredients: { type: [String], required: true },
-    instructions: { type: String, required: true },
-    category: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
+  title: { type: String, required: true },
+  ingredients: { type: [String], required: true },
+  instructions: { type: String, required: true },
+  category: { type: String, required: true },
+  prepTime: { type: String, required: true }, 
+  cookTime: { type: String, required: true }, 
+  servings: { type: String, required: true }, 
+  picture: { type: String, required: false }, 
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
